c88bc04d8a0c09d5f4e4be920dad3c6b5483b8bb
Deepak Kumar Sahu <125984730+deepakkumar1211@users.noreply.github.com>
2024-04-03 10:32:33 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valInString = "33abs"
+let valInNumber = Number(valInString)
+
+console.log(valInNumber);
+console.log(typeof valInNumber);
+
+// "33" => 33
+// "33abc" => NaN -> Not a Number
+// true => 1, false => 0
+
+// 1 => true, 0 => false
+// "" => false
+// "Deepak" => true
+
+
