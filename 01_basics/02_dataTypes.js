68c0daa285aaed05bf61adbbf5f42bd1035732d0
Deepak Kumar Sahu <125984730+deepakkumar1211@users.noreply.github.com>
2024-04-03 08:44:44 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all JS code as newer version
+
+// Data Types
+let name = "Deepak"
+let age = 15
+let isLoggedin = false
+
+// number = 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => varible define but value not assign
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
