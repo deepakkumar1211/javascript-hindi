3c70fd17020742fd2362409099e34c4a5d98a1d0
Deepak Kumar Sahu <125984730+deepakkumar1211@users.noreply.github.com>
2024-04-09 18:25:06 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,14 @@
 let score = 33
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
 
 let valInString = "33abs"
 let valInNumber = Number(valInString)
-
-console.log(valInNumber);
-console.log(typeof valInNumber);
+// console.log(valInNumber);
+// console.log(typeof valInNumber);
 
 // "33" => 33
 // "33abc" => NaN -> Not a Number
@@ -16,5 +17,29 @@ console.log(typeof valInNumber);
 // 1 => true, 0 => false
 // "" => false
 // "Deepak" => true
+// -------------------00------------------------
+
+
+// 07 Why string to number conversion is difficult
+str1 = "hello "
+str2 = "Deepak"
+str3 = str1+str2
+// console.log(str3);
+
+
+// console.log("1" + "2");
+// console.log(1 + "2");
+// console.log("1" + 2);
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + 2 + "2" + 1 + 3);
+// integer pahle rhega to add hoga otherwise nhi
+
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
 
 
